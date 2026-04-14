940aa024f74a2f199384ea65aa4dc68f2738d7dc
shafrinahamed0007 <mrdeveloperdevv@gmail.com>
2026-04-14 18:52:45 +0600

    Declare loading fallbal using react loading spinner

--- src/components/homepage/TrendingApps.jsx
+++ src/components/homepage/TrendingApps.jsx
@@ -1,7 +1,7 @@
 import React, { use, useEffect, useState } from "react";
 
 import AppCard from "../ui/AppCard";
-
+import { ClockLoader } from "react-spinners";
 
 // import { useLoaderData } from "react-router";
 
@@ -14,31 +14,41 @@ const TrendingApps = () => {
   //   console.log("Data from Trending Apps vaya home page", data);
 
   const [apps, setApps] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
       const res = await fetch("/data.json");
       const data = await res.json();
       console.log("Data fetching using use effect", data);
-      setApps(data);
+      setTimeout(() => {
+        setApps(data);
+        setLoading(false);
+      }, 2000);
     };
     fetchData();
   }, []);
 
   console.log("Apps Data", apps);
+  console.log("Loading", loading);
 
   return (
     <div>
       <div>
         {/* Section header */}
         Total Apps: {apps.length}
-        <div className="grid md:grid-cols-2 lg:grid-cols-3 container mx-auto">
-          {apps.map((app) => {
-            return (
-              <AppCard key={app.id} app={app} />
-            );
-          })}
-        </div>
+        {loading ? (
+          <div className="flex justify-center items-center">
+            {" "}
+            <ClockLoader color="#c92222" />
+          </div>
+        ) : (
+          <div className="grid md:grid-cols-2 lg:grid-cols-3 container mx-auto">
+            {apps.map((app) => {
+              return <AppCard key={app.id} app={app} />;
+            })}
+          </div>
+        )}
       </div>
       <div className="mb-8 text-center">
         <h2 className="font-bold text-4xl">Trending Apps</h2>
