130eaee7539b1bc44888d4734ef76b04fd5617e9
shafrinahamed0007 <mrdeveloperdevv@gmail.com>
2026-04-14 12:15:52 +0600

    declare navlink using map

--- src/components/shared/MyNavLink.jsx
+++ src/components/shared/MyNavLink.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import { NavLink } from "react-router";
 
-const MyNavLink = ({to, children}) => {
+const MyNavLink = ({ to, children }) => {
   return (
-    <NavLink
-      to={to}
-      className={({ isActive }) =>
-        `font-semibold rounded-lg p-2 ${isActive ? "bg-[#00313C] underline underline-offset-1" : ""}`
-      }
-    >{children}</NavLink>
+    <div>
+      <NavLink
+        to={to}
+        className={({ isActive }) =>
+          `font-semibold rounded-lg p-2 ${isActive ? "bg-[#00313c] underline underline-offset-1" : ""}`
+        }
+      >
+        {children}
+      </NavLink>
+    </div>
   );
 };
 
--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -5,22 +5,37 @@ import { FaGithub } from "react-icons/fa";
 import MyNavLink from "./MyNavLink";
 
 const Navbar = () => {
+  const navItem = [
+    {
+      path: "/",
+      text: "Home",
+    },
+    {
+      path: "/apps",
+      text: "Apps",
+    },
+    {
+      path: "/installApps",
+      text: "Installation",
+    },
+  ];
   return (
     <div className=" shadow ">
       <nav className="flex justify-between gap-4 items-center  py-4 container mx-auto ">
         <img src={logo} alt="" className="max-w-[40px]" />
-        <ul className="flex justify-between gap-4 items-center">
-          <li>
-            <MyNavLink to={"/"}>Home</MyNavLink>
-          </li>
-          <li>
-            <MyNavLink to={"/apps"}>Apps</MyNavLink>
-          </li>
-          <li>
-            <MyNavLink to={"/installApps"}>Installation</MyNavLink>
-          </li>
-          
+        <ul className="flex justify-between gap-2 items-center">
+          {
+            navItem.map((item) =>(
+              <MyNavLink to={item.path}>{item.text}</MyNavLink>
+            ))
+          }
+
         </ul>
+        {/* <ul className="flex justify-between gap-4 items-center">
+          <MyNavLink to={"/"}>Home</MyNavLink>
+          <MyNavLink to={"/apps"}>Apps</MyNavLink>
+          <MyNavLink to={"/installApps"}>Installation</MyNavLink>
+        </ul> */}
 
         <button className="btn btn-neutral">
           <FaGithub /> Contribute
