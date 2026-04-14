e6c62811b301e47f927bc1583c6d584c316a64f9
shafrinahamed0007 <mrdeveloperdevv@gmail.com>
2026-04-14 13:58:26 +0600

    data fetch usenig useLoader Hook

--- src/main.jsx
+++ src/main.jsx
@@ -18,10 +18,12 @@ const router = createBrowserRouter([
         path: "/",
         index: true,
         element: <Homepage />,
+        loader: ()=>fetch("/data.json")
       },
       {
         path: "/apps",
         element: <Apps />,
+        
       },
       {
         path: "/installApps",
--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -1,14 +1,17 @@
-import React from 'react';
-import Banner from '../../components/homepage/Banner';
-import Stats from '../../components/homepage/Stats';
-import TrendingApps from '../../components/homepage/TrendingApps';
+import React from "react";
+import Banner from "../../components/homepage/Banner";
+import Stats from "../../components/homepage/Stats";
+import TrendingApps from "../../components/homepage/TrendingApps";
+import { useLoaderData } from "react-router";
 
 const Homepage = () => {
+  const data = useLoaderData();
+  console.log("Data from home home page", data);
   return (
     <>
-    <Banner />
-    <Stats />
-    <TrendingApps />
+      <Banner />
+      <Stats />
+      <TrendingApps />
     </>
   );
 };
